82e28b7b0e54c59061ffd0f99e8bb330ce210425
Dilshodbek <20020905xd@gmail.com>
2022-08-14 00:08:05 +0500

    eslint disabled

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
   images: {
     domains: ["geografiya.uz", "s3-alpha-sig.figma.com"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
