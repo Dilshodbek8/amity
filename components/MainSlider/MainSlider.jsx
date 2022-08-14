9a4aff836d0374bdd1cc6dad02034ac078a31d9b
Dilshodbek <20020905xd@gmail.com>
2022-08-14 20:47:22 +0500

    partners debugged

--- components/MainSlider/MainSlider.jsx
+++ components/MainSlider/MainSlider.jsx
@@ -1,8 +1,8 @@
 import classes from "./MainSlider.module.scss";
 import Slider from "react-slick";
-import SliderCard from "../SliderCard/SliderCard";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import SliderCard from "../SliderCard/SliderCard";
 
 const MainSlider = ({ className, data }) => {
   const settings = {
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { useTranslation } from "next-i18next";
 import data from "../fakedata/home.json";
 import MainSlider from "../components/MainSlider/MainSlider";
@@ -10,7 +13,46 @@ import HomeNews from "../components/HomeNews/HomeNews";
 import InfoCard from "../components/InfoCard/InfoCard";
 import Image from "next/image";
 import HomeContact from "../components/HomeContact/HomeContact";
-
+const settings = {
+  dots: false,
+  arrows: false,
+  autoplay: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 5,
+  autoplaySpeed: 2000,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 400,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
 export default function Home() {
   const { t } = useTranslation();
 
@@ -61,15 +103,17 @@ export default function Home() {
         </div>
       </div>
       <div className={classes["home__partners"]}>
-        {data.partners.map((partner, idx) => (
-          <div key={idx} className={classes["home__partners__image"]}>
-            <Image
-              src={`/media/images/${partner}`}
-              layout="fill"
-              alt="partner"
-            />
-          </div>
-        ))}
+        <Slider {...settings}>
+          {data.partners.map((partner, idx) => (
+            <div key={idx} className={classes["home__partners__image"]}>
+              <Image
+                src={`/media/images/${partner}`}
+                layout="fill"
+                alt="partner"
+              />
+            </div>
+          ))}
+        </Slider>
       </div>
       <div className={classes["home__contact"]}>
         <HomeContact />
